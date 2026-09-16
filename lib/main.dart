064f18cc494fc993f7b3b963a67ac748a0255d4c
zixow <thibaultnathanps5@gmail.com>
2026-09-17 10:42:40 +1100

    application mobile

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('You have pushed the button this many times:'),
+            const Text('Application mobile de Office du tourisme'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
